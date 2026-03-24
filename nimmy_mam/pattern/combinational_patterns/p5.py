d7819d17de689076a20ca92a7078e201f0789208
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-03-24 10:25:55 +0530

    python...

--- nimmy_mam/pattern/combinational_patterns/p5.py
+++ nimmy_mam/pattern/combinational_patterns/p5.py
@@ -158,7 +158,20 @@ print("=================================")
 n=int(input("enter the number :"))
 noc=n
 for i in range(1,n*2):
-    for j in range(n,noc+1):
+    for j in range(n,noc-1,-1):
+            print(j,end="")
+    print()
+    if i<n:
+          noc-=1
+    else:
+          noc+=1
+
+print("=================================")
+
+n=int(input("enter the number :"))
+noc=n
+for i in range(1,n*2):
+    for j in range(1,noc+1):
             print(j,end="")
     print()
     if i<n:
--- nimmy_mam/pattern/combinational_patterns/p6.py
+++ nimmy_mam/pattern/combinational_patterns/p6.py
@@ -1,4 +1,4 @@
-n=int(input("enter the number : "))
+'''n=int(input("enter the number : "))
 for i in range(n,(1-1),-1):
     for j in range(i,(1-1),-1):
         print(j,end="")
@@ -48,4 +48,53 @@ for i in range(1,n+1):
             print("1",end="")
         else:
             print("0",end="")
+    print()
+print("==============================")'''
+
+n=int(input("enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i <j:
+            print(i,end=" ")
+        else:
+            print(j,end=" ")
+    print()
+print("==============================")
+
+n=int(input("enter the number: "))
+for i in range(n,(1-1),-1):
+    for j in range(n,(1-1),-1):
+        if i <j:
+            print(i,end=" ")
+        else:
+            print(j,end=" ")
+    print()
+print("==============================")
+
+n=int(input("enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i >j:
+            print(i,end=" ")
+        else:
+            print(j,end=" ")
+    print()
+print("==============================")
+
+n=int(input("enter the number: "))
+for i in range(n,(1-1),-1):
+    for j in range(n,(1-1),-1):
+        if i >j:
+            print(i,end=" ")
+        else:
+            print(j,end=" ")
+    print()
+print("======================")
+
+n=int(input("enter the number: "))
+count=1
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(count,end=" ")
+        count+=1
     print()
