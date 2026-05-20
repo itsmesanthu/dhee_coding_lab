543d4ae991090139cc21110eb1a0f416926b7ce1
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-05-20 14:13:03 +0530

    :)

---
+++ nimmy_mam/strong_number.py
@@ -0,0 +1,37 @@
+# def strong(n):
+#     temp=n
+#     sum=0
+#     while n>0:
+#         a=n%10
+#         f=1
+#         for i in range(1,a+1):
+#             f*=i
+#         sum+=f
+#         n=n//10
+#     return temp==sum
+# n=int(input("entre number:"))
+# flag=strong(n)
+# if flag:
+#     print(f"the number {n} is strong number ")
+# else:
+#      print(f"the number {n} is not a  strong number ")
+def strong(n):
+    temp=n
+    sum=0
+    while n>0:
+        a=n%10
+        f=factor(a)
+        sum+=f
+        n=n//10
+    return temp==sum
+def factor(n):
+    f=1
+    for i in range(1,n+1):
+        f*=i
+    return f
+n=int(input("entre number:"))
+flag=strong(n)
+if flag:
+    print(f"the number {n} is strong number ")
+else:
+     print(f"the number {n} is not a  strong number ")
