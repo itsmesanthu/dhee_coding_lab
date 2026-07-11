b235acbdb5c15e05e1ef40074f91b83d36e5e3c3
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-07-11 08:40:24 +0530

    ..

---
+++ nimmy_mam/Linked_list/Floyds_Cycle_Detection_Algorithm.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+    def middle_node(self,head):
+        s=head
+        f=head
+        while f and f.next:
+            s=s.next
+            f=f.next.next
+            if s==f:
+                return True
+        return False
+    
+n1=Node(10)
+n2=Node(20)
+n3=Node(30)
+n4=Node(40)
+n5=Node(50)
+n6=Node(70)
+n1.next=n2
+n2.next=n3
+n3.next=n4
+n4.next=n5
+n5.next=n6
+# n6.next=n4
+l=Node(n1)
+res=l.middle_node(n1)
+print(res)
---
+++ nimmy_mam/array/Bubbel_sort.py
@@ -0,0 +1,10 @@
+arr=[10,7,6,5,20,-1,-2]
+def bubbel_sort(arr):
+    n=len(arr)
+    for i in range(0,n):
+        for j in range(0,n-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+res=bubbel_sort(arr)
+print(res)
