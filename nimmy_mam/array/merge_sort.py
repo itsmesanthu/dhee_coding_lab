c9da40461829e3f06a982b1ecda90961a72d9104
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-07-17 09:42:27 +0530

    ..

--- nimmy_mam/array/merge_sort.py
+++ nimmy_mam/array/merge_sort.py
@@ -1,46 +1,38 @@
-def createIntArray():
-    l1=[]
-    while True:
-        try:
-            n=int(input("enter a val: "))
-            l1.append(n)
-        except Exception as e:
-            return l1
-def mergesortmergin(arr,start,mid,end):
-    i,j=start,mid+1
-    res=[]
-    for k in range(0,((mid+end)+1)):
-        if i<=mid and j<=end:
-            if arr[i]<=arr[j]:
-                res.append(arr[i])
-                i+=1
-            else:
-                res.append(arr[j])
-                j+=1
-        else:# for extar value
-            if i<=mid:
-                res.append(arr[i])
-                i+=1
-                k+=1
-            elif j<=end:
-                res.append(arr[j])
-                j+=1
-                k+=1
-    #updating the original memory from res[]
-    for k in range(0,len(res)):
-        arr[start]=res[k]
-        start+=1
-def mergesortdivision(arr,start,end):
-    if start>=end:
-        return
-    mid=(start+end)//2
-    #LHS division
-    mergesortdivision(arr,start,mid)
-    #RHS division
-    mergesortdivision(arr,mid+1,end)
-    mergesortmergin(arr,start,mid,end)
+def merge(a, left, mid, right):
+    i = left
+    j = mid + 1
+    k = 0
 
-arr=createIntArray()
-print("orignal array :",arr)
-mergesortdivision(arr,0,(len(arr)-1))
-print("asc sored arr: ", arr)
+    c = [0] * (right - left + 1)
+
+    while i <= mid and j <= right:
+        if a[i] <= a[j]:
+            c[k] = a[i]
+            i += 1
+        else:
+            c[k] = a[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        c[k] = a[i]
+        i += 1
+        k += 1
+
+    while j <= right:
+        c[k] = a[j]
+        j += 1
+        k += 1
+
+    for i in range(len(c)):
+        a[left + i] = c[i]
+
+def mergesort(a,left,right):
+    if(left<right):
+        mid=(left+right)//2
+        mergesort(a,left,mid)
+        mergesort(a,mid+1,right)
+        merge(a,left,mid,right)
+a=[100,-5,6,20,21,7,5,9,11,5]
+mergesort(a,0,len(a)-1)
+print(a)
